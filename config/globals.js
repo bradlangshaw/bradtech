ec8b7f458b09757f43194dbc200296b2e65aa6db
Brad Langshaw <200243850@student.georgianc.on.ca>
2017-05-15 12:58:47 -0400

    Sdfg

--- config/globals.js
+++ config/globals.js
@@ -1,10 +1,4 @@
 module.exports = {
   // db: 'mongodb://localhost/comp2106-w2017' // local mongodb (no auth)
-   db: 'mongodb://bradlangs:langbrad1@ds135800.mlab.com:35800/bradlangshawtech',
-    google: {
-       clientID: '67497188487-4qqtkan2m955b35eelgfu9efkdi1u14b.apps.googleusercontent.com',
-       clientSecret: 'CgSDr7zJ61TWgR-FkGSEmrUv',
-        callbackURL: 'https://www.bradlngshaw.tech/google/callback'
-       // callbackURL: 'http://localhost:3000/google/callback'
-    }
+   db: 'mongodb://bradlangs:langbrad1@ds135800.mlab.com:35800/bradlangshawtech'
 };
